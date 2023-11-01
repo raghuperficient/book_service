493639b0186a0c56a0b88cff315715d156cd98d1
rookiecoder <mvraghu@hotmail.com>
2023-11-01 11:44:00 -0700

    updating cors

--- src/main/java/com/test/controller/BookController.java
+++ src/main/java/com/test/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://books-git-raghu-demo.apps.prft-cps.zuvk.p1.openshiftapps.com/")
 @RestController
 @RequestMapping("/api")
 public class BookController {
